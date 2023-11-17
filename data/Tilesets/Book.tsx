161e520ead2bd9afe236016989415e148bb9d17f
QuanHoang1204 <149780652+QuanHoang1204@users.noreply.github.com>
2023-11-17 12:27:00 +0700

    First commit

---
+++ data/Tilesets/Book.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Book" tilewidth="250" tileheight="225" tilecount="1" columns="1">
+ <image source="../../graphics/book/0.png" width="250" height="225"/>
+</tileset>
